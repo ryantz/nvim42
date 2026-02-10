206c0819496692bb9ce2ed38b3bd22fbb2b07250
Ryan Zhiyang Tan <ryatan@c2r6s14.42singapore.sg>
2026-02-10 13:56:45 +0800

    latest

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 -- =========================
 --   Minimal NeoVim for 42
 -- =========================
-
 -- Install lazy.nvim (plugin manager)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -18,59 +17,70 @@ require("lazy").setup({
     -- Telescope fuzzy finder
     { "nvim-lua/plenary.nvim" },
     { "nvim-telescope/telescope.nvim" },
- 	{ "lewis6991/gitsigns.nvim",
+	{ "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup {}
     end
-   	},
+  	},
 
     -- 42 header
     {
         "42Paris/42header",
-  		config = function()
-  		 vim.g.user42 = "ryatan"
-  		 vim.g.mail42 = "ryatan@student.42singapore.sg"
-  		end
+		config = function()
+			vim.g.user42 = "ryatan"
+			vim.g.mail42 = "ryatan@student.42singapore.sg"
+		end
     },
- 	{
-    	"nvim-treesitter/nvim-treesitter",
-    	build = ":TSUpdate",
-    	config = function()
-    	  require'nvim-treesitter.configs'.setup {
-    	    ensure_installed = { "c", "lua", "python", "bash" },
-    	    highlight = { enable = true },
-    	  }
-    	end,
- 	},
 	{
-  		'RostislavArts/naysayer.nvim',
-  		priority = 1000,
-  		lazy = false,
+  		"nvim-treesitter/nvim-treesitter",
+  		build = ":TSUpdate",
   		config = function()
-  		  vim.cmd.colorscheme('naysayer')
+  		  require'nvim-treesitter.configs'.setup {
+  		    ensure_installed = { "c", "lua", "python", "bash" },
+  		    highlight = { enable = true },
+  		  }
   		end,
 	},
 	{
-    	'nvim-lualine/lualine.nvim',
-    	dependencies = { 'nvim-tree/nvim-web-devicons' },
-		config = function()
-			require('lualine').setup {
-				options = {
-					theme = '16color',
-					section_separators = ' ',
-					component_separators = '',
-				}
-			}
-		end
+		 'RostislavArts/naysayer.nvim',
+		 priority = 1000,
+		 lazy = false,
+		 config = function()
+		   vim.cmd.colorscheme('naysayer')
+		 end,
+	},
+	{
+    	'Verf/deepwhite.nvim',
+    	lazy = false,
+    	priority = 1000,
+    	config = function()
+    	    vim.cmd [[colorscheme deepwhite]]
+    	end,
+	},
+	{ "shortcuts/no-neck-pain.nvim" },
+	{
+	  "oskarnurm/koda.nvim",
+	  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	  priority = 1000, -- make sure to load this before all the other start plugins
+	  config = function()
+		  vim.cmd("colorscheme koda")
+	  end,
+	},
+	{
+	  "yorickpeterse/vim-paper",
+	  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	  priority = 1000, -- make sure to load this before all the other start plugins
+	  config = function()
+		  vim.cmd("colorscheme paper")
+	  end,
 	},
 })
 
 -- =========================
 -- Editor Settings
 -- =========================
-vim.opt.termguicolors = true
---vim.opt.background = "light"
-vim.cmd.colorscheme("naysayer")
+vim.o.background = "light"
+vim.cmd.colorscheme("paper")
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:block,r-cr-o:block"
 
 -- Line at 80 chars (42 norm)
@@ -94,11 +104,18 @@ vim.opt.number = true
 vim.g.mapleader = " "
 
 -- Telescope
-vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<CR>", { desc = "Find files" })
+vim.keymap.set("n", "<leader>sf", "<cmd>Telescope find_files<CR>", { desc = "Find files" })
 vim.keymap.set("n", "<leader>g", "<cmd>Telescope live_grep<CR>", { desc = "Live grep" })
-
--- 42 Header
-vim.keymap.set("n", "<leader>h", "<cmd>Stdheader<CR>", { desc = "Insert 42 header" })
 
 -- go back to ex
 vim.keymap.set("n", "<leader>e", ":Ex<CR>", { desc = "Open Netrw file explorer" })
+
+-- no neck pain
+vim.keymap.set("n", "<leader>nn", ":NoNeckPain<CR>", { desc = "No neck pain" })
+
+-- splits
+vim.keymap.set("n", "<leader>\\", ":vsplit<CR>", { desc = "vertical split in nvim" })
+vim.keymap.set("n", "<leader>-", ":split<CR>", { desc = "horizontal split in nvim" })
+vim.keymap.set("n", "<leader>c", "<C-w>c", { desc = "close split in nvim" })
+
+
